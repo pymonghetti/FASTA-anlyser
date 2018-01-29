90e26fcb1c5fa3f3f86ae03b6060c808c74610b7
Simon New <35465288+pythonghetti@users.noreply.github.com>
2018-01-29 10:42:59 +0000

    Update fasta_analyser.py

--- fasta_analyser.py
+++ fasta_analyser.py
@@ -1,33 +1,14 @@
-from tkinter import *
+sequence = input("Enter your sequence here: ") #User input
 
-root = Tk()
-root.title("FASTA analyser")
-topframe = Frame(root)
+caps = sequence.upper() #uppercase sequence - FASTA files could potentially be mixed upper and lower.
+length = len(sequence)
 
-MH_numbers_label = Label(root, text="Copy and paste FASTA sequence here:")
-MH_numbers_label.pack(padx=2, pady=2)
+accepted_bases = ('G', 'C', 'T', 'A') #no ambiguity codes included yet
 
-MH_number_entry = Entry(root)
-MH_number_entry.pack(padx=2, pady=2)
+print("Sequence analysed:")
+print("Sequence length =",length,"bp")
 
-button1 = Button(root, text="Find sequences", bg="lightgreen")
-button1.pack(padx=2, pady=2)
-
-
-
-#sequence = input("Enter your sequence here: ") #User input
-
-#caps = sequence.upper() #uppercase sequence - FASTA files could potentially be mixed upper and lower.
-#length = len(sequence)
-
-#accepted_bases = ('G', 'C', 'T', 'A') #no ambiguity codes included yet
-
-#print("Sequence analysed:")
-#print("Sequence length =",length,"bp")
-
-#for bases in accepted_bases:
-    #count = caps.count(bases)
-    #content = round(((count/length)*100), 2)
-    #print(bases, "content is", content, "%")
-
-root.mainloop()
+for bases in accepted_bases:
+    count = caps.count(bases)
+    content = round(((count/length)*100), 2)
+    print(bases, "content is", content, "%")
